2c230550e4486ae2eae1ecf37d82c4b414f86679
AndreasZacchi <zacchiandreas@gmail.com>
2022-11-03 23:13:10 +0100

    Fixed a drawing issue
    Issue made some edges be drawn double as well as edges having a looped edge

--- sketch.js
+++ sketch.js
@@ -3,17 +3,30 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   textSize(10);
   textAlign(CENTER, CENTER);
-  setFrameRate(1);
 
-  vertexAmount = 10;
-  nodeAmount = 2 * vertexAmount;
+  vertexAmount = 50;
+  edgesAmount = 2 * vertexAmount;
   vertex = [];
   edges = [];
   for (let i = 0; i < vertexAmount; i++) {
     vertex.push(new Vertex(i));
   }
-  for (let i = 0; i < vertexAmount; i++) {
-    edges.push(new Edge(random(vertex), random(vertex), int(random(80))));
+  for (let i = 0; i < edgesAmount; i++) {
+    let v1 = random(vertex);
+    let v2 = random(vertex);
+
+    while (v1 == v2) {
+      v2 = random(vertex);
+    }
+
+    let check = edges.filter(
+      (edge) =>
+        (edge.src === v1 && edge.target === v2) ||
+        (edge.target === v1 && edge.src === v2)
+    );
+    if (typeof check !== "undefined" && check.length === 0) {
+      edges.push(new Edge(v1, v2, int(random(80))));
+    }
   }
 
   g = new Graph(vertex, edges);
